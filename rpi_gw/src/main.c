a6a016168cc242f9598ea99c136ccce8dc40b75d
cipriponta <ponta_ciprian@yahoo.com>
2023-10-14 02:49:37 +0300

    Started socket programming for rpi_gw

--- rpi_gw/src/main.c
+++ rpi_gw/src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <sys/socket.h>
 
 int main()
 {
-    printf("SANKI\n");
+    int server_file_descriptor;
+    int option = 1; 
+
+    server_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_file_descriptor < 0)
+    {
+        printf("Could not create a new socket\n");
+    }
+
+    if(0 == setsockopt(server_file_descriptor, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option)))
+    {
+        printf("Could not modify the socket");
+    }
+
     return 0;
 }
