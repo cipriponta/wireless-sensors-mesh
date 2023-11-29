93d04508f6d7e27b3f4254f8c599f17b814124ad
Ciprian Ponta <ponta_ciprian@yahoo.com>
2023-11-29 04:06:04 +0200

    Added the pc-gui component

---
+++ pc-gui/pc-gui/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace pc_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbGasSensorValue = new System.Windows.Forms.TextBox();
+            this.tbTmpSensorValue = new System.Windows.Forms.TextBox();
+            this.tbFanState = new System.Windows.Forms.TextBox();
+            this.lbGasSensorValue = new System.Windows.Forms.Label();
+            this.lbTmpSensorValue = new System.Windows.Forms.Label();
+            this.lbFanState = new System.Windows.Forms.Label();
+            this.lbConnection = new System.Windows.Forms.Label();
+            this.tbConnection = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbGasSensorValue
+            // 
+            this.tbGasSensorValue.Location = new System.Drawing.Point(178, 62);
+            this.tbGasSensorValue.Name = "tbGasSensorValue";
+            this.tbGasSensorValue.ReadOnly = true;
+            this.tbGasSensorValue.Size = new System.Drawing.Size(100, 23);
+            this.tbGasSensorValue.TabIndex = 0;
+            this.tbGasSensorValue.Text = "0";
+            // 
+            // tbTmpSensorValue
+            // 
+            this.tbTmpSensorValue.Location = new System.Drawing.Point(178, 103);
+            this.tbTmpSensorValue.Name = "tbTmpSensorValue";
+            this.tbTmpSensorValue.ReadOnly = true;
+            this.tbTmpSensorValue.Size = new System.Drawing.Size(100, 23);
+            this.tbTmpSensorValue.TabIndex = 1;
+            this.tbTmpSensorValue.Text = "0.0";
+            // 
+            // tbFanState
+            // 
+            this.tbFanState.Location = new System.Drawing.Point(178, 142);
+            this.tbFanState.Name = "tbFanState";
+            this.tbFanState.ReadOnly = true;
+            this.tbFanState.Size = new System.Drawing.Size(100, 23);
+            this.tbFanState.TabIndex = 2;
+            this.tbFanState.Text = "Off";
+            // 
+            // lbGasSensorValue
+            // 
+            this.lbGasSensorValue.AutoSize = true;
+            this.lbGasSensorValue.Location = new System.Drawing.Point(75, 65);
+            this.lbGasSensorValue.Name = "lbGasSensorValue";
+            this.lbGasSensorValue.Size = new System.Drawing.Size(95, 15);
+            this.lbGasSensorValue.TabIndex = 3;
+            this.lbGasSensorValue.Text = "Gas Sensor Value";
+            // 
+            // lbTmpSensorValue
+            // 
+            this.lbTmpSensorValue.AutoSize = true;
+            this.lbTmpSensorValue.Location = new System.Drawing.Point(28, 106);
+            this.lbTmpSensorValue.Name = "lbTmpSensorValue";
+            this.lbTmpSensorValue.Size = new System.Drawing.Size(142, 15);
+            this.lbTmpSensorValue.TabIndex = 4;
+            this.lbTmpSensorValue.Text = "Temperature Sensor Value";
+            // 
+            // lbFanState
+            // 
+            this.lbFanState.AutoSize = true;
+            this.lbFanState.Location = new System.Drawing.Point(115, 145);
+            this.lbFanState.Name = "lbFanState";
+            this.lbFanState.Size = new System.Drawing.Size(55, 15);
+            this.lbFanState.TabIndex = 5;
+            this.lbFanState.Text = "Fan State";
+            // 
+            // lbConnection
+            // 
+            this.lbConnection.AutoSize = true;
+            this.lbConnection.Location = new System.Drawing.Point(105, 21);
+            this.lbConnection.Name = "lbConnection";
+            this.lbConnection.Size = new System.Drawing.Size(65, 15);
+            this.lbConnection.TabIndex = 6;
+            this.lbConnection.Text = "Connected";
+            // 
+            // tbConnection
+            // 
+            this.tbConnection.Location = new System.Drawing.Point(178, 18);
+            this.tbConnection.Name = "tbConnection";
+            this.tbConnection.ReadOnly = true;
+            this.tbConnection.Size = new System.Drawing.Size(100, 23);
+            this.tbConnection.TabIndex = 7;
+            this.tbConnection.Text = "No";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(88, 187);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(128, 23);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 222);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbConnection);
+            this.Controls.Add(this.lbConnection);
+            this.Controls.Add(this.lbFanState);
+            this.Controls.Add(this.lbTmpSensorValue);
+            this.Controls.Add(this.lbGasSensorValue);
+            this.Controls.Add(this.tbFanState);
+            this.Controls.Add(this.tbTmpSensorValue);
+            this.Controls.Add(this.tbGasSensorValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbGasSensorValue;
+        private System.Windows.Forms.TextBox tbTmpSensorValue;
+        private System.Windows.Forms.TextBox tbFanState;
+        private System.Windows.Forms.Label lbGasSensorValue;
+        private System.Windows.Forms.Label lbTmpSensorValue;
+        private System.Windows.Forms.Label lbFanState;
+        private System.Windows.Forms.Label lbConnection;
+        private System.Windows.Forms.TextBox tbConnection;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ pc-gui/pc-gui/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+
+namespace pc_gui
+{
+    public partial class Form1 : Form
+    {
+        private const string SERVER_ADDRESS = "raspberrypi.local";
+        private const int SERVER_PORT = 8443;
+        private const string CERTIFICATE_RELATIVE_PATH = "certs/rpi_cert.pem";
+
+        private TcpClient client = null;
+        private SslStream sslStream = null;
+        private Thread updateTaskHandler = null;
+        private System.Windows.Forms.Timer swTimer = null;
+
+        private bool isConnected = false;
+        private string gasSensorValue = "0";
+        private string tmpSensorValue = "0.0";
+        private string fanState = "Off";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StoreSSLCertificate();
+            updateTaskHandler = new Thread(new ThreadStart(UpdateTask));
+
+            swTimer = new System.Windows.Forms.Timer();
+            swTimer.Interval = 1000; // each second
+            swTimer.Tick += new EventHandler(UIUpdateTask);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (client != null)
+            {
+                client.Close();
+            }
+            if (sslStream != null)
+            {
+                sslStream.Close();
+            }
+            if (updateTaskHandler != null)
+            {
+                updateTaskHandler.Join();
+            }
+            if(swTimer.Enabled)
+            {
+                swTimer.Stop();
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient(SERVER_ADDRESS, SERVER_PORT);
+            sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+            try
+            {
+                sslStream.AuthenticateAsClient(SERVER_ADDRESS);
+                isConnected = true;
+                btnConnect.Enabled = false;
+                swTimer.Start();
+                updateTaskHandler.Start();
+            }
+            catch
+            {
+                
+            }
+        }
+
+        private void UpdateTask()
+        {
+            if (sslStream != null)
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024];
+                    sslStream.Read(buffer, 0, buffer.Length);
+                    string decodedBuffer = Encoding.UTF8.GetString(buffer).TrimEnd('\0');
+
+                    Dictionary<String, Object> jsonOutput = JsonSerializer.Deserialize<Dictionary<String, Object>>(decodedBuffer);
+                    foreach(KeyValuePair<String, Object> entry in jsonOutput)
+                    {
+                        if("gas_sensor_value" == entry.Key)
+                        {
+                            gasSensorValue = entry.Value.ToString();
+                        }
+                        else if ("tmp_sensor_value" == entry.Key)
+                        {
+                            tmpSensorValue = entry.Value.ToString();
+                        }
+                        else if("fan_state" == entry.Key)
+                        {
+                            fanState = ("True" == entry.Value.ToString()) ? "On" : "Off";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UIUpdateTask(object sender, EventArgs e)
+        {
+            tbConnection.Text = isConnected ? "Yes" : "No";
+            tbGasSensorValue.Text = gasSensorValue;
+            tbTmpSensorValue.Text = tmpSensorValue;
+            tbFanState.Text = fanState;
+        }
+
+        private bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return sslPolicyErrors == SslPolicyErrors.None;
+        }
+
+        private void StoreSSLCertificate()
+        {
+            string currentPath = System.IO.Directory.GetCurrentDirectory();
+            string certificatePath = String.Format("{0}/../../../../../{1}", currentPath, CERTIFICATE_RELATIVE_PATH);
+
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(new X509Certificate2(certificatePath));
+            store.Close();
+        }
+    }
+}
